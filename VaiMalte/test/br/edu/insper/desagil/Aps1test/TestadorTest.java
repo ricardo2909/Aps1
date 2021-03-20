07f267ae8f9505349a66bb539220a10a8143ef51
ricardo2909 <ricardo2909@users.noreply.github.com>
2021-03-20 17:11:06 -0300

    inicio

---
+++ VaiMalte/test/br/edu/insper/desagil/Aps1test/TestadorTest.java
@@ -0,0 +1,47 @@
+package br.edu.insper.desagil.Aps1test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import br.edu.insper.desagil.Aps1.Testador;
+
+class TestadorTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+	private Testador t;
+
+	@BeforeEach
+	private void setUp() {
+	    t = new Testador();
+	}
+
+	@Test
+	public void testeA() {
+	    assertTrue(t.testeA());
+	}
+
+	@Test
+	public void testeB() {
+	    assertTrue(t.testeB());
+	}
+
+	@Test
+	public void testeC() {
+	    assertTrue(t.testeC());
+	}
+
+	@Test
+	public void testeD() {
+	    assertTrue(t.testeD());
+	}
+
+	@Test
+	public void testeE() {
+	    assertTrue(t.testeE());
+	}
+}
